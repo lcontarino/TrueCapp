daa12812c0dd1c31cc28a261d118101db340d6bf
lcontarino <lcontarino@gmail.com>
2022-05-30 23:21:06 -0300

    Index

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
         <BrowserRouter>
           <NavBar />
           <Routes>
-            <Route path='/' element={<ItemListContainer greeting="Todos los Productos"/>}/>
+            <Route path='/TrueCapp' element={<ItemListContainer greeting="Todos los Productos"/>}/>
             {/* <Route path='/category/:categoryId' element={<ItemListContainer greeting="Productos filtrados por categoria"/>}/> */}
             <Route path='/detail/:productId' element={<ItemDetailContainer />}/>
-            <Route path='/about' element={<h1>About</h1>}/>
+            <Route path='/products' element={<h1>Products</h1>}/>
             <Route path='*' element={<h1>PAGE NOT FOUND 404</h1>} />
           </Routes>
         </BrowserRouter>
